341bd7b6c29a28cdfb7fc8c12f87b33af311397b
eanp <awifamz@gmail.com>
2023-09-18 11:39:18 +0700

    change path seeder

--- src/controller/Airlines.ts
+++ src/controller/Airlines.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from "express";
 import { PrismaClient } from "@prisma/client";
 import response from '../helpers/Common'
 import { CustomRequest } from '../helpers/Token'
-import { airport,airline } from "../../lib/seeder";
+import { airport,airline } from "../seeder/seeder";
 import { v4 as uuidv4 } from 'uuid';
 const prisma = new PrismaClient()
 
